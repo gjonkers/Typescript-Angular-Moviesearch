61f348b4bdaf88f6824bff305bdfbedcb59898b2
Prasad Korrapati <Prasad Korrapati>
2017-03-08 10:20:58 +0530

    Created interface to make the searchMovies service typestrict

--- app/js/movies/services/movieService.ts
+++ app/js/movies/services/movieService.ts
@@ -1,20 +1,23 @@
 /// <reference path="../../../../typings/angularjs/angular.d.ts" />
 /// <reference path="../../../../typings/angularjs/angular-route.d.ts" />
 
-class movieService {
+interface IMovie {
+    searchMovies(movieString: string): ng.IPromise<{}>; 
+}
+
+class movieService implements IMovie  {
     static $inject = ["$http"];
     public httpService: ng.IHttpService;
     constructor($http: ng.IHttpService) {
         this.httpService = $http;
     }
 
-    public searchMovies(movieString) {
+    searchMovies(movieString:string) : ng.IPromise<{}> {
         console.log("search triggered");
         return this.httpService.get("http://www.omdbapi.com/?s=" + movieString);
     }
 
 }
-
 
 // Register the controller with Angular
 angular.module('moviesModule').service('movieService',movieService);
